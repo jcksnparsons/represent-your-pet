e6dd190c72b40da42467adefb78e44b8e265ea02
Jack Parsons <jcksnparsons@yahoo.com>
2020-01-20 13:22:10 -0600

    added play behavior to cormac

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const cormac = {
     species: "Dog",
     nickname: ["Big Mac", "Corm", "Goofy"],
     age: 10,
+    favoriteToys: [],
     bark: function () {
         window.alert("WOOF!")
     },
@@ -11,5 +12,13 @@ const cormac = {
     },
     sigh: function () {
         window.alert("siiiiiiiiigh")
-    }
+    },
+    play: function (toy) {
+        if (toy.includes("squeaky")) {
+            this.favoriteToys.push(toy);
+            window.alert(`Cormac played with ${toy}!`)
+        } else {
+            window.alert("Cormac wasn't interested...")
+        }
+}
 }
