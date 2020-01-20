b21473f6aa727c0441d363c398c268d97897d204
Jack Parsons <jcksnparsons@yahoo.com>
2020-01-20 12:46:49 -0600

    added methods to cormac object

--- main.js
+++ main.js
@@ -2,5 +2,14 @@ const cormac = {
     name: "Cormac",
     species: "Dog",
     nickname: ["Big Mac", "Corm", "Goofy"],
-    age: 10
+    age: 10,
+    bark: function () {
+        window.alert("WOOF!")
+    },
+    grumble: function () {
+        window.alert("grumble grumble...")
+    },
+    sigh: function () {
+        window.alert("siiiiiiiiigh")
+    }
 }
